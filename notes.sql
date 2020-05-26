8f48c792b6e711975fd23d946e313d27959cc131
Christian Hall <christian.hall395@gmail.com>
2020-05-26 15:43:23 -0400

    second-final update

---
+++ notes.sql
@@ -0,0 +1,5 @@
+select * from user; -- you will write this somewhere in java
+-- produces a result set, there is a java object that can interact with the result set and display
+-- you create an class for the tables, in which the rows are objects. 
+-- you are taking the data from these virtual tables and rmatching them up with java objects and commands ORM(Object relational structores)
+-- explore chater 20 to learn about SQL lite
